cfe3e618bc983c46f36c8b8abfcdaa441753ee94
suellen <suellenflickr@gmail.com>
2024-08-14 18:20:07 -0300

    add pagination params

--- src/routers/equipment.py
+++ src/routers/equipment.py
@@ -2,13 +2,14 @@ from contextlib import closing
 from flask import request
 from flask_restx import Resource
 from flask_smorest import Blueprint
+from flask_sqlalchemy.query import Query as BaseQuery
 from http import HTTPStatus
 
 from src.config import db
 from src.helpers import LogHelper
 from src.logs import logger
 from src.models import Equipment, EquipmentSchema
-from src.routers.helpers import configure_session, get_response
+from src.routers.helpers import get_response
 
 equipment_blueprint = Blueprint("Equipment", __name__)
 
@@ -16,30 +17,28 @@ equipment_blueprint = Blueprint("Equipment", __name__)
 @equipment_blueprint.route("/equipment")
 class RouteEquipment(Resource):
     def get(self):
-        with closing(configure_session()) as session:
-            try:
-                result = session.query(Equipment)\
-                    .order_by(Equipment.equipmentId) \
-                    .all()
+        try:
+            query = db.session.query(Equipment)
 
-                total_count = len(result)
+            result = get_rows_paginated(query)
 
-                if not result:
-                    return get_response(HTTPStatus.OK, {'total': total_count,
-                                                        'equipments': result,
-                                                        'message': 'No equipment was found'})
+            total_count = query.count()
 
-                logger.info('get all equipments')
-
+            if not result:
                 return get_response(HTTPStatus.OK, {'total': total_count,
-                                                    'equipments': EquipmentSchema(many=True).dump(result),
-                                                    'message': 'Request happened successfully'})
-            except Exception as ex:
-                session.rollback()
-                msg = f'Unable to get equipment list. Error: {str(ex)}'
-                log_msg = LogHelper.get_log_msg(msg, request)
-                logger.exception(log_msg)
-                return get_response(HTTPStatus.INTERNAL_SERVER_ERROR, msg)
+                                                    'equipments': result,
+                                                    'message': 'No equipment was found'})
+
+            logger.info('get all equipments')
+
+            return get_response(HTTPStatus.OK, {'total': total_count,
+                                                'equipments': EquipmentSchema(many=True).dump(result),
+                                                'message': 'Request happened successfully'})
+        except Exception as ex:
+            msg = f'Unable to get equipment list. Error: {str(ex)}'
+            log_msg = LogHelper.get_log_msg(msg, request)
+            logger.exception(log_msg)
+            return get_response(HTTPStatus.INTERNAL_SERVER_ERROR, msg)
 
     def post(self):
         body = request.get_json() if request.get_json() else dict()
@@ -65,3 +64,14 @@ class RouteEquipment(Resource):
         db.session.commit()
         logger.info(f'Category created: {new_equipment}')
         return get_response(HTTPStatus.CREATED, EquipmentSchema().dump(new_equipment))
+
+
+def get_rows_paginated(query: BaseQuery):
+    page = int(request.args.get('page')) if request.args.get('page') else 1
+    per_page = int(request.args.get('per_page')
+                   ) if request.args.get('per_page') else 100
+
+    result: Equipment = query.order_by(Equipment.equipmentId).paginate(
+        page=page, per_page=per_page).items
+
+    return result
